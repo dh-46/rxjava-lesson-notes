1889c9821d0637cde499ad91c914ccf9b7a977b5
Daniel Huang <danielhuang.work@gmail.com>
2020-10-31 16:13:27 +0800

    Ch1: 引用RxJava

---
+++ src/main/java/tw/dh46/rxjava/Main.java
@@ -0,0 +1,37 @@
+package tw.dh46.rxjava;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+
+/**
+ * 測試RxJava引用
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Observable.just("Hello RxJava!")
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        System.out.println("onSubscribe");
+                    }
+
+                    @Override
+                    public void onNext(@NonNull String s) {
+                        System.out.println("onNext: " + s);
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("onComplete");
+                    }
+                });
+    }
+}
